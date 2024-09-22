80ed86cd8baf23a0f693de10d0463ade64f7d8c0
Vivek Varma Pusapati <varmavivek77@gmail.com>
2024-09-22 11:46:05 +0800

    Updated seeder to insert 100 mil usernames into DB. Updated readme.

--- readme.md
+++ readme.md
@@ -4,3 +4,13 @@
 ```
 docker compose up
 ```
+
+## Notes
+### SimpleSearch 
+Uses username as the primary key. Searching 100 mil records only takes 1-2 ms. 
+#### Advantages:
+1. Search is quick
+2. insert time duplicate check not required
+#### Drawbacks:
+1. Changing a user's username is complex. Update all references of the old primary key in all the tables/databases with the new primary key.
+2. What if username need not be unique. 
--- seeds/pg_simple_username_seeder.go
+++ seeds/pg_simple_username_seeder.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -11,8 +13,13 @@ import (
 )
 
 func (s Seed) PgSimpleUsernameSeed() {
-	absPath, _ := filepath.Abs("./data/xato-net-10-million-usernames.txt")
-	readFile, err := os.Open(absPath)
+	absPathUsernames, _ := filepath.Abs("./data/xato-net-10-million-usernames.txt")
+	absPathFamilynames, _ := filepath.Abs("./data/familynames.txt")
+	readUsernamesFile, err := os.Open(absPathUsernames)
+	if err != nil {
+		fmt.Println(err)
+	}
+	readFamilynamesFile, err := os.Open(absPathFamilynames)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -23,7 +30,15 @@ func (s Seed) PgSimpleUsernameSeed() {
 			fmt.Fprintf(os.Stderr, "Exception when closing file: %v\n", err)
 			os.Exit(1)
 		}
-	}(readFile)
+	}(readUsernamesFile)
+
+	defer func(readFile *os.File) {
+		err := readFile.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Exception when closing file: %v\n", err)
+			os.Exit(1)
+		}
+	}(readFamilynamesFile)
 
 	dropTableString := fmt.Sprintf("DROP TABLE IF EXISTS simple;")
 	createTableString := fmt.Sprintf("CREATE TABLE simple (username varchar(255) PRIMARY KEY);")
@@ -40,40 +55,57 @@ func (s Seed) PgSimpleUsernameSeed() {
 		os.Exit(1)
 	}
 
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
-
 	start := time.Now()
+
+	familynamesFileScanner := bufio.NewScanner(readFamilynamesFile)
+	familynamesFileScanner.Split(bufio.ScanLines)
 	counter := 0
 	avgCounter := 0
 	batchSize := 1000
-	batchStart := time.Now()
-	usernames := make([]any, 0, batchSize)
-	valuesPlaceholder := make([]string, 0, batchSize)
-	for fileScanner.Scan() {
-		counter++
-		username := fileScanner.Text()
-		usernames = append(usernames, username)
-		valuesPlaceholder = append(valuesPlaceholder, fmt.Sprintf("($%d)", counter%batchSize+1))
 
-		if counter%batchSize == 0 {
-			avgCounter++
-			insertUsernameString := fmt.Sprintf("INSERT INTO simple (%s) VALUES %s;", "username", strings.Join(valuesPlaceholder, ", "))
-			_, err := s.db.Exec(context.Background(), insertUsernameString, usernames...)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Failed to insert username %v due to %v", username, err.Error())
-				if !strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
-					os.Exit(1)
+	for familynamesFileScanner.Scan() {
+		familyname := familynamesFileScanner.Text()
+		fmt.Printf("FAMILYNAME: %v", familyname)
+		batchStart := time.Now()
+		usernames := make([]any, 0, batchSize)
+		valuesPlaceholder := make([]string, 0, batchSize)
+
+		_, err = readUsernamesFile.Seek(0, io.SeekStart)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var usernamesFileScanner *bufio.Scanner = nil
+		usernamesFileScanner = bufio.NewScanner(readUsernamesFile)
+		usernamesFileScanner.Split(bufio.ScanLines)
+
+		batchIndex := 0
+		for usernamesFileScanner.Scan() {
+			counter++
+			batchIndex++
+			username := fmt.Sprintf("%v%v", usernamesFileScanner.Text(), strings.ToLower(familyname))
+			usernames = append(usernames, username)
+			valuesPlaceholder = append(valuesPlaceholder, fmt.Sprintf("($%d)", batchIndex%batchSize+1))
+
+			if batchIndex%batchSize == 0 {
+				avgCounter++
+				insertUsernameString := fmt.Sprintf("INSERT INTO simple (%s) VALUES %s;", "username", strings.Join(valuesPlaceholder, ", "))
+				_, err := s.db.Exec(context.Background(), insertUsernameString, usernames...)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Failed to insert username %v due to %v", username, err.Error())
+					if !strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+						os.Exit(1)
+					}
 				}
+				fmt.Printf("Inserted %d records\n", counter)
+				if avgCounter%batchSize == 0 {
+					fmt.Printf("Took average of %v to insert %d records into database\n", time.Now().Sub(batchStart)/1000, batchSize)
+					batchStart = time.Now()
+				}
+				usernames = nil
+				valuesPlaceholder = nil
 			}
-			fmt.Printf("Inserted %d records\n", counter)
-			if avgCounter%batchSize == 0 {
-				fmt.Printf("Took average of %v to insert %d records into database\n", time.Now().Sub(batchStart)/1000, batchSize)
-				batchStart = time.Now()
-			}
-			usernames = nil
-			valuesPlaceholder = nil
 		}
 	}
-	fmt.Printf("Took total of %v to insert 10 mil records into database\n", time.Now().Sub(start))
+	fmt.Printf("Took total of %v to insert %d records into database\n", time.Now().Sub(start), counter)
 }
